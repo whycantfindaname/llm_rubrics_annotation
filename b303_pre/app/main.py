4c898f7fd85ca71a4fa8f236c001ff2931ebf296
jasonliao.21 <jasonliao.21@bytedance.com>
2026-01-23 03:11:48 +0800

    feat: update main.py and middleware.py

--- b303_pre/app/main.py
+++ b303_pre/app/main.py
@@ -2,9 +2,11 @@
 
 from fastapi import FastAPI
 from .routes import router
+from .middleware import logging_middleware
 
 app = FastAPI()
 app.include_router(router)
+app.middleware("http")(logging_middleware)
 
 @app.get("/")
 def root():
